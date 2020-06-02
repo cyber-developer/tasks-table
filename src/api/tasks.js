261ee2364fca9081cbd23d5383122e753af050fd
cyber-developer <cyber.devworld@gmail.com>
2020-06-02 19:28:00 +0500

    data separated

---
+++ src/api/tasks.js
@@ -0,0 +1,74 @@
+export const DATA = [
+  {
+    id: 1,
+    source: 'Lead',
+    name: 'Dream Folks',
+    date: 'Mar 19, 2020',
+    confId: 'SE7103',
+    submittedBy: 'Admin',
+    accountId: '00SD15S700S121000154',
+    billingStreet: '2335 N. Michigan Avenue',
+    leadSource: 'Morganville',
+    billingState: 'New York'
+  },
+  {
+    id: 2,
+    source: 'Account',
+    name: 'Johnson and Co',
+    date: 'Mar 20, 2020',
+    confId: 'SE7104',
+    submittedBy: 'Admin',
+    accountId: '00SD15S700S121000154',
+    billingStreet: '2335 N. Michigan Avenue',
+    leadSource: 'Morganville',
+    billingState: 'New York'
+  },
+  {
+    id: 3,
+    source: 'Lead',
+    name: 'Dream Folks',
+    date: 'Mar 19, 2020',
+    confId: 'SE7103',
+    submittedBy: 'Admin',
+    accountId: '00SD15S700S121000154',
+    billingStreet: '2335 N. Michigan Avenue',
+    leadSource: 'Morganville',
+    billingState: 'New York'
+  },
+  {
+    id: 4,
+    source: 'Account',
+    name: 'Johnson and Co',
+    date: 'Mar 20, 2020',
+    confId: 'SE7104',
+    submittedBy: 'Admin',
+    accountId: '00SD15S700S121000154',
+    billingStreet: '2335 N. Michigan Avenue',
+    leadSource: 'Morganville',
+    billingState: 'New York'
+  },
+  {
+    id: 5,
+    source: 'Lead',
+    name: 'Dream Folks',
+    date: 'Mar 19, 2020',
+    confId: 'SE7103',
+    submittedBy: 'Admin',
+    accountId: '00SD15S700S121000154',
+    billingStreet: '2335 N. Michigan Avenue',
+    leadSource: 'Morganville',
+    billingState: 'New York'
+  },
+  {
+    id: 6,
+    source: 'Account',
+    name: 'Johnson and Co',
+    date: 'Mar 20, 2020',
+    confId: 'SE7104',
+    submittedBy: 'Admin',
+    accountId: '00SD15S700S121000154',
+    billingStreet: '2335 N. Michigan Avenue',
+    leadSource: 'Morganville',
+    billingState: 'New York'
+  }
+]
